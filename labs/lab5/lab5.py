76c85acc9f0755d75e4c21d73ad4408a56c6438e
ullmanjd <89550070+ullmanjd@users.noreply.github.com>
2021-09-28 15:57:28 -0400

    Add files via upload

--- labs/lab5/lab5.py
+++ labs/lab5/lab5.py
@@ -1,86 +1,169 @@
-"""
-Name: <your name goes here – first and last>
-<ProgramName>.py
-"""
+# Jacob Ullman
+# Lab5
+#
+from math import sqrt
 
-from graphics import *
-
-
-def target():
-    win_width = 500
-    win_height = 500
-    win = GraphWin("Archery Target", win_width, win_height)
-
-    # Add code here to get the dimensions and draw the target
-
-    # Wait for another click to exit
-    win.getMouse()
-    win.close()
+from graphics import GraphWin, Point, Text, Polygon, color_rgb, Circle, Entry
 
 
 def triangle():
-    win_width = 500
-    win_height = 500
-    win = GraphWin("Draw a Triangle", win_width, win_height)
+    # Creates a graphical window
+    width = 400
+    height = 400
+    graphics = GraphWin("Lab 5", width, height)
 
-    # Add code here to accept the mouse clicks, draw the triangle.
-    # and display its area in the graphics window.
+    # number of times user can create points
+    num_clicks = 3
 
-    # Wait for another click to exit
-    win.getMouse()
-    win.close()
+    # create a space to instruct user
+    inst_pt = Point(width / 2, height - 10)
+    instructions = Text(inst_pt, "Click 3 times to set the triangle's corners.")
+    instructions.draw(graphics)
+
+    # allows the user to click multiple times to set the triangle
+    for i in range(num_clicks):
+        p1 = graphics.getMouse()
+        p2 = graphics.getMouse()
+        p3 = graphics.getMouse()
+        tri = Polygon(p1, p2, p3)
+        tri.setOutline("blue")
+        tri.setFill("black")
+        tri.draw(graphics)
+
+        dx1 = p1.getX() - p2.getX()
+        dx2 = p2.getX() - p3.getX()
+        dx3 = p3.getX() - p1.getX()
+        dy1 = p1.getY() - p2.getY()
+        dy2 = p2.getY() - p3.getY()
+        dy3 = p3.getY() - p1.getY()
+        l1 = sqrt(dx1 ** 2 + dy1 ** 2)
+        l2 = sqrt(dx2 ** 2 + dy2 ** 2)
+        l3 = sqrt(dx3 ** 2 + dy3 ** 2)
+        perimeter = l1 + l2 + l3
+        s = perimeter / 2
+        perimeter = str(perimeter)
+        display = Text(inst_pt, "The perimeter is " + perimeter)
+        instructions.undraw()
+        display.draw(graphics)
+
+        area_pt = Point(width / 2, height - 30)
+        area = sqrt(s * (s - l1) * (s - l2) * (s - l3))
+        area = str(area)
+        display2 = Text(area_pt, "The area is " + area)
+        display2.draw(graphics)
+    graphics.getMouse()
+    graphics.close()
 
 
 def color_shape():
-    '''Create code to allow a user to color a shape by entering rgb amounts'''
+    # Creates a graphical window
+    width = 400
+    height = 400
+    graphics = GraphWin("Lab 5", width, height)
 
-    # create window
-    win_width = 400
-    win_height = 400
-    win = GraphWin("Color Shape", win_width, win_height)
-    win.setBackground("white")
+    # number of times user can alter RGB values
+    num_rgb = 5
 
-    # create text instructions
-    msg = "Enter color values between 0 - 255\nClick window to color shape"
-    inst = Text(Point(win_width / 2, win_height - 20), msg)
-    inst.draw(win)
+    # create a space to instruct user
+    inst_pt = Point(width / 2, height - 10)
+    instructions = Text(inst_pt, "Enter a number between 0-255 for Red Blue and Green.")
+    instructions.draw(graphics)
 
-    # create circle in window's center
-    shape = Circle(Point(win_width / 2, win_height / 2 - 30), 50)
-    shape.draw(win)
+    # builds a circle
+    shape = Circle(Point(200, 100), 100)
+    shape.setOutline("black")
+    shape.setFill(color_rgb(0, 0, 0))
+    shape.draw(graphics)
 
-    # redTexPt is 50 pixels to the left and forty pixels down from center
-    red_text_pt = Point(win_width / 2 - 50, win_height / 2 + 40)
-    red_text = Text(red_text_pt, "Red: ")
-    red_text.setTextColor("red")
+    r_pt = Point(width / 2, height - 50)
+    g_pt = Point(width / 2, height - 70)
+    b_pt = Point(width / 2, height - 90)
 
-    # green_text_pt is 30 pixels down from red
-    green_text_pt = red_text_pt.clone()
-    green_text_pt.move(0, 30)
-    green_text = Text(green_text_pt, "Green: ")
-    green_text.setTextColor("green")
+    rbox = Entry(r_pt, 30)
+    gbox = Entry(g_pt, 30)
+    bbox = Entry(b_pt, 30)
 
-    # blue_text_pt is 60 pixels down from red
-    blue_text_pt = red_text_pt.clone()
-    blue_text_pt.move(0, 60)
-    blue_text = Text(blue_text_pt, "Blue: ")
-    blue_text.setTextColor("blue")
+    rbox.draw(graphics)
+    gbox.draw(graphics)
+    bbox.draw(graphics)
 
-    # display rgb text
-    red_text.draw(win)
-    green_text.draw(win)
-    blue_text.draw(win)
+    # allows the user to set the RGB values 5 times
+    for i in range(num_rgb):
+        graphics.getMouse()
+        r = rbox.getText()
+        g = gbox.getText()
+        b = bbox.getText()
+        r = eval(r)
+        g = eval(g)
+        b = eval(b)
 
-    # Wait for another click to exit
-    win.getMouse()
-    win.close()
+        shape.setFill(color_rgb(r, g, b))
 
-
-def main():
-    # target()
-    # triangle()
-    # color_shape()
-    pass
+    graphics.getMouse()
+    graphics.close()
 
 
-main()
+def process_string():
+    userinput = eval(input("Please enter text: "))
+    first = userinput[0]
+    print(first)  # first letter
+    last = userinput[-1]
+    print(last)  # last letter
+    twotofive = userinput[2, 6]
+    print(twotofive)  # letters in pos 2-5
+    firstlast = first + last
+    print(firstlast)  # first and last letter
+    firstthree = userinput[0, 3]
+    firstthree = firstthree + firstthree + firstthree + firstthree + firstthree + firstthree + firstthree + firstthree + firstthree + firstthree
+    print(firstthree)  # first three letters
+    l = len(userinput)
+    for i in range(0, l):
+        count = 0
+        print(userinput[count])  # each letter printed individually
+        count = count + 1
+    print(l)  # number of characters
+
+
+def process_list():
+    pt = Point(5, 10)
+    values = [5, 'hi', 2.5, 'there', pt, '7.2']
+    l = len(values)
+    x = values[1] + values[3]
+    print(x)
+    x = values[0] + values[2]
+    print(x)
+    x = values[1] + values[1] + values[1] + values[1] + values[1]
+    print(x)
+    x = [values[2], values[3], pt]
+    print(x)
+    x = [values[2], values[3], 5]
+    print(x)
+    x = [values[2], values[0], values[-1]]
+    print(x)
+    x = values[2] + values[0] + values[-1]
+    print(x)
+    x = l
+    print(x)
+
+
+def another_series():
+    userinput = eval(input("Please enter number of terms: "))
+    l = userinput
+    sumint = 0
+    for i in range(0, l):
+        count = 1
+        sums = [0,l]
+        if count == 3:
+            sums[i] = 6
+            count = 1
+            sumint = sumint + sums[i]
+        if count == 2:
+            sums[i] = 4
+            count = 3
+            sumint = sumint + sums[i]
+        if count == 1:
+            sums[i] = 2
+            count = 2
+            sumint = sumint + sums[i]
+        print(sumint)
+
